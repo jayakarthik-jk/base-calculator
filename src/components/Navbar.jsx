ced347e4fc864322a791c120365df0e790cca1c5
Jayakarthik <karthick72002@gmail.com>
2022-09-05 23:52:22 +0530

    Initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+    <nav className="navbar">
+      <span className="brand">Base Calculator</span>
+      <a href="https://github.com/jayakarthik-jk" className="author">
+        Jayakarthik.jk
+      </a>
+    </nav>
+  );
+}
+
+export default Navbar;
